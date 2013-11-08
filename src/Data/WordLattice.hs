42a983d71fb925def7c9c080769fa0b243e44b5f
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-11-08 22:02:31 +0100

    Current snapshot

--- src/Data/WordLattice.hs
+++ src/Data/WordLattice.hs
@@ -9,160 +9,187 @@ module Data.WordLattice
 ( 
 -- * Types
   Lattice
-, NodeID
-, EdgeID
 , Node (..)
+, NodeID
 , Edge (..)
+, EdgeID
 
--- * Query
--- ** Node
-, nodes
-, nodes'
-, inGo
-, outGo
-, nodeVal
--- ** Edge
-, edges
-, edges'
-, beg
-, end
-, edgeVal
+, topSort
+, nodesAsc
+, nodesDesc
 
--- * Map
-, mapN
-, mapE
-, mapT
-, mapT'
+-- -- * Query
+-- -- ** Node
+-- , nodes
+-- , nodes'
+-- , inGo
+-- , outGo
+-- , nodeVal
+-- -- ** Edge
+-- , edges
+-- , edges'
+-- , beg
+-- , end
+-- , edgeVal
+-- 
+-- -- * Map
+-- , mapN
+-- , mapE
+-- , mapT
+-- , mapT'
 ) where
 
 
--- -- | A word lattice is a directed acyclic graph (DAG) with words assigned to
--- -- individual edges.  Nodes, on the other hand, can be used to represent spaces
--- -- between words.
--- --
--- -- A word lattice can be used to represent an ambiguous sentence segmentation
--- -- (arising from a morphological analysis, for example) and it has been
--- -- developed with this application in mind.  On the other hand, it should
--- -- be also possible to use a lattice to represent a set of sentences.
--- data WordLattice = WordLattice
--- 
--- 
--- -- | A node of a word lattice.
--- data Node = Node {
---     -- | 
---     space    :: T.Text }
--- 
--- 
--- -- | An edge of a word lattice.
--- data Edge = Edge
-
-
--- | A lattice is a directed acyclic graph (DAG) with values of type `a`
--- assigned to individual nodes and values of type `b` assigned to individual
--- edges.
-data Lattice a b = Lattice
-
-
-instance Functor (Lattice a) where
-    fmap = mapE
+import qualified Data.IntMap.Strict as I
+import qualified Data.Vector.Unboxed as U
 
 
 -- | A node identifier.
-type Node = Int
+type NodeID = Int
 
 
 -- | An edge identifier.
-type Edge = Int
+type EdgeID = Int
 
 
-------------------------------------------------------------------------------
--- Node queries
-------------------------------------------------------------------------------
+-- | A node.
+data Node a = Node
+    { valN  :: a
+    , ing   :: U.Vector EdgeID
+    , out   :: U.Vector EdgeID }
 
 
--- | Return DAG nodes in a topological order, starting at the beginning of a
--- sentence and ending where the sentence ends.
-nodes :: Lattice a b -> [Node]
-nodes = undefined
+-- | An edge.
+data Edge a = Edge
+    { valE  :: a
+    , beg   :: NodeID
+    , end   :: NodeID }
 
 
--- | Return DAG nodes in a reverse topological order.
-nodes' :: Lattice a b -> [Node]
-nodes' = undefined
+-- | A lattice is a directed acyclic graph (DAG) with values of type
+-- `a` assigned to individual nodes and values of type `b` assigned
+-- to individual edges.
+data Lattice a b = Lattice {
+    -- | Map of nodes.
+      nodeMap  :: I.IntMap (Node a)
+    -- | Map of edges.
+    , edgeMap  :: I.IntMap (Edge b) }
 
 
--- | Get the list of ingoing edges.
-inGo :: Lattice a b -> Node -> [Edge]
-inGo = undefined
+-- | Perform topological sort of the lattice nodes.
+topSort :: Lattice a b -> Lattice a b
+topSort = error "topSort: not implemented"
 
 
--- | Get the list of outgoing edges.
-outGo :: Lattice a b -> Node -> [Edge]
-outGo = undefined
+-- | Return nodes in an ascending order w.r.t. the node identifiers.
+-- See also `topSort`.
+nodesAsc :: Lattice a b -> [NodeID]
+nodesAsc = map fst . I.toAscList . nodeMap
 
 
--- | Get the value assigned to a particular node.
-nodeVal :: Lattice a b -> Node -> a
-nodeVal = undefined
-
-
-------------------------------------------------------------------------------
--- Edge queries
-------------------------------------------------------------------------------
-
-
--- | Return DAG edges in a topological order.  Similar to `nodes`.
-edges :: Lattice a b -> [Edge]
-edges = undefined
-
-
--- | Return DAG edges in a reverse topological order.  Similar to `nodes'`.
-edges' :: Lattice a b -> [Edge]
-edges' = undefined
-
-
--- | Beginning node of the edge.
-beg :: Lattice a b -> Edge -> Node
-beg = undefined
-
-
--- | Ending node of the edge.
-end :: Lattice a b -> Edge -> Node
-end = undefined
-
-
--- | Get the value assigned to a particular node.
-edgeVal :: Lattice a b -> Edge -> b
-edgeVal = undefined
-
-
-------------------------------------------------------------------------------
--- Mapping
-------------------------------------------------------------------------------
-
-
--- | Map function over the node values.
-mapN :: (a -> c) -> Lattice a b -> Lattice c b
-mapN = undefined
-
-
--- | Map function over the edge values (consider using `fmap` instead).
-mapE :: (b -> c) -> Lattice a b -> Lattice a c
-mapE = undefined
+-- | Return nodes in an descending order w.r.t. the node identifiers.
+-- See also `topSort`.
+nodesDesc :: Lattice a b -> [NodeID]
+nodesDesc = map fst . I.toDescList . nodeMap
+-- NOTE: containers 0.5.X are needed, because older versions don't
+-- provide the `I.toDescList` function.
 
 
 -- | A traversal-like mapping function which can be used to map a given
 -- function over edge values.  While mapping the elementary function over
--- a particular edge, the function has access to mapping results for
--- all the preceding edges in the lattice.
---
--- WARNING: if the elementary function tries to access a node, which
--- doesn't precede the current node, an error will be raised.
-mapT :: (Lattice a c -> b -> c) -> Lattice a b -> Lattice a c
-mapT = undefined
+-- a particular edge, the function has access to mapping results for all
+-- preceding edges in the lattice.
+-- mapT :: (Lattice a c -> b -> c) -> Lattice a b -> Lattice a c
 
 
--- | A traversal-like mapping function similar to `mapT`, with the difference
--- that the lattice is traversed in the opposite direction.
-mapT' :: (Lattice a c -> b -> c) -> Lattice a b -> Lattice a c
-mapT' = undefined
+-- -------------------------------------------------------------------
+-- -- Node queries
+-- -------------------------------------------------------------------
+-- 
+-- 
+-- -- | Return DAG nodes in a topological order, starting at the beginning
+-- -- of a sentence and ending where the sentence ends.
+-- nodes :: Lattice a b -> [Node]
+-- nodes = undefined
+-- 
+-- 
+-- -- | Return DAG nodes in a reverse topological order.
+-- nodes' :: Lattice a b -> [Node]
+-- nodes' = undefined
+-- 
+-- 
+-- -- | Get the list of ingoing edges.
+-- inGo :: Lattice a b -> Node -> [Edge]
+-- inGo = undefined
+-- 
+-- 
+-- -- | Get the list of outgoing edges.
+-- outGo :: Lattice a b -> Node -> [Edge]
+-- outGo = undefined
+-- 
+-- 
+-- -- | Get the value assigned to a particular node.
+-- nodeVal :: Lattice a b -> Node -> a
+-- nodeVal = undefined
+-- 
+-- 
+-- ------------------------------------------------------------------------------
+-- -- Edge queries
+-- ------------------------------------------------------------------------------
+-- 
+-- 
+-- -- | Return DAG edges in a topological order.  Similar to `nodes`.
+-- edges :: Lattice a b -> [Edge]
+-- edges = undefined
+-- 
+-- 
+-- -- | Return DAG edges in a reverse topological order.  Similar to `nodes'`.
+-- edges' :: Lattice a b -> [Edge]
+-- edges' = undefined
+-- 
+-- 
+-- -- | Beginning node of the edge.
+-- beg :: Lattice a b -> Edge -> Node
+-- beg = undefined
+-- 
+-- 
+-- -- | Ending node of the edge.
+-- end :: Lattice a b -> Edge -> Node
+-- end = undefined
+-- 
+-- 
+-- -- | Get the value assigned to a particular node.
+-- edgeVal :: Lattice a b -> Edge -> b
+-- edgeVal = undefined
+-- 
+-- 
+-- ------------------------------------------------------------------------------
+-- -- Mapping
+-- ------------------------------------------------------------------------------
+-- 
+-- 
+-- -- | Map function over the node values.
+-- mapN :: (a -> c) -> Lattice a b -> Lattice c b
+-- mapN = undefined
+-- 
+-- 
+-- -- | Map function over the edge values (consider using `fmap` instead).
+-- mapE :: (b -> c) -> Lattice a b -> Lattice a c
+-- mapE = undefined
+-- 
+-- 
+-- -- | A traversal-like mapping function which can be used to map a given
+-- -- function over edge values.  While mapping the elementary function over
+-- -- a particular edge, the function has access to mapping results for
+-- -- all preceding edges in the lattice.
+-- --
+-- -- WARNING: if the elementary function tries to access a node, which
+-- -- doesn't precede the current node, an error will be raised.
+-- mapT :: (Lattice a c -> b -> c) -> Lattice a b -> Lattice a c
+-- mapT = undefined
+-- 
+-- 
+-- -- | A traversal-like mapping function similar to `mapT`, with the difference
+-- -- that the lattice is traversed in the opposite direction.
+-- mapT' :: (Lattice a c -> b -> c) -> Lattice a b -> Lattice a c
+-- mapT' = undefined
